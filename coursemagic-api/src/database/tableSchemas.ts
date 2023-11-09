853e962fd56a46c030685d9cd11f2f297d1fd80c
Eddy Ding <edwarding04@gmail.com>
2023-11-09 16:20:33 -0500

    Refactor sql table schemas

--- coursemagic-api/src/database/tableSchemas.ts
+++ coursemagic-api/src/database/tableSchemas.ts
@@ -3,38 +3,36 @@ import sql from "./sql";
 const tables = async () => {
   await sql`
     CREATE TABLE IF NOT EXISTS users (
-      id varchar(255) UNIQUE PRIMARY KEY,
+      id INT UNIQUE PRIMARY KEY,
       name varchar(255) NOT NULL
     );
-
-    ${/* Connects userids to certain classids which represent as users currently selected classes n*/""}
-    CREATE TABLE IF NOT EXISTS userCurrentClasses (
-      id SERIAL PRIMARY KEY,
-      userid varchar(255) NOT NULL,
-      classid varchar(255) NOT NULL,
-      FOREIGN KEY (userid) REFERENCES users(id),
-      FOREIGN KEY (classid) REFERENCES classes(id)
-    );
-
-    ${/* ID of this collectionSavedClasses is how you get the savedClasses that are from a collection n*/""}
-    CREATE TABLE IF NOT EXISTS collectionSavedClasses (
-      id SERIAL PRIMARY KEY,
-      userid varchar(255) NOT NULL,
-      FOREIGN KEY (userid) REFERENCES users(id),
-    );
-    
-    ${/* Individual classes saved in some collection. Includes reference to this collection*/""}
-    CREATE TABLE IF NOT EXISTS savedClasses (
-      id SERIAL PRIMARY KEY,
-      collectionid varchar(255) NOT NULL,
-      classid varchar(255) NOT NULL,
-      FOREIGN KEY (collectionid) REFERENCES collectionSavedClasses(id),
-      FOREIGN KEY (classid) REFERENCES classes(id)
-    );
-
+  `
+  await sql`
     CREATE TABLE IF NOT EXISTS classes (
       id SERIAL PRIMARY KEY,
-      userid varchar(255) NOT NULL,
+      userid INT NOT NULL,
+      FOREIGN KEY (userid) REFERENCES users(id)
+    );
+  `
+  /* Connects userids to certain classids which represent as users currently selected classes n*/
+  await sql`
+    CREATE TABLE IF NOT EXISTS userCurrentClasses (
+      id SERIAL PRIMARY KEY,
+      userid INT NOT NULL,
+      classid INT NOT NULL,
+      FOREIGN KEY (userid) REFERENCES users(id),
+      FOREIGN KEY (classid) REFERENCES classes(id)
+    );
+  `
+  /* Individual classes saved in some collection. Includes reference to this collection*/
+  await sql`
+    CREATE TABLE IF NOT EXISTS savedClasses (
+      id SERIAL PRIMARY KEY,
+      userid INT NOT NULL,
+      collectionName varchar(255) NOT NULL,
+      classid INT NOT NULL,
+      UNIQUE(userid, classid, collectionName),
+      FOREIGN KEY (classid) REFERENCES classes(id),
       FOREIGN KEY (userid) REFERENCES users(id)
     );
   `
